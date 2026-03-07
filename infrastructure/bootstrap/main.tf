e49228031d74cece2751e155bd29d07f948921a3
Dewey jose <richard.e.jose@gmail.com>
2026-03-07 12:31:39 -0500

    asdf

--- infrastructure/bootstrap/main.tf
+++ infrastructure/bootstrap/main.tf
@@ -373,6 +373,7 @@ data "aws_iam_policy_document" "github_actions_permissions" {
       "iam:DetachRolePolicy",
       "iam:ListRolePolicies",
       "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfilesForRole",
       "iam:GetPolicy",
       "iam:GetPolicyVersion",
       "iam:ListPolicyVersions",
